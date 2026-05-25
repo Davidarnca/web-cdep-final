0df13017eecd4be5795bf84d28cf4295b05ee9f1
David Ardila <davidar25@gmail.com>
2026-05-24 21:50:26 -0500

    Fix: Importación de CSS en main.tsx y compatibilidad CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import "./styles.css"; // O la ruta donde esté tu archivo de estilos de Tailwind
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
